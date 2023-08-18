26b8c72f1374443fb3979e601a10d76191692504
Zackaria Antar <antarzack@gmail.com>
2023-08-18 11:55:09 -0500

    added logs to pooljs heroku dbms troubleshooting.

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,3 +1,5 @@
+// import { createClient } from "@supabase/supabase-js";
+
 /* the only line you likely need to change is
 
  database: 'prime_app',
@@ -5,7 +7,7 @@
  change `prime_app` to the name of your database, and you should be all set!
 */
 
-const pg = require('pg');
+const pg = require("pg");
 let pool;
 
 // When our app is deployed to the internet
@@ -13,23 +15,29 @@ let pool;
 // to set the connection info: web address, username/password, db name
 // eg:
 //  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
+console.log("MADE IT TO POOLJS");
 if (process.env.DATABASE_URL) {
-    pool = new pg.Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: true
-        }
-    });
+    console.log('FOUND DATABASE URL');
+	pool = new pg.Pool({
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	});
 }
 // When we're running this app on our own computer
 // we'll connect to the postgres database that is
 // also running on our computer (localhost)
 else {
-    pool = new pg.Pool({
+	pool = new pg.Pool({
 		host: "localhost",
 		port: 5432,
 		database: "low_key_oke_base", // 	💥 Change this to the name of your database!
 	});
 }
+
+// const supabaseUrl = "https://nurcizukdintkioijqwi.supabase.co";
+// const supabaseKey = process.env.DATABASE_URL;
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = pool;
