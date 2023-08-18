ae879c60308e0866d7475b8d0e2ed06276f72d64
Zackaria Antar <antarzack@gmail.com>
2023-08-18 10:27:09 -0500

    changed ssl to true on pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -17,7 +17,7 @@ if (process.env.DATABASE_URL) {
     pool = new pg.Pool({
         connectionString: process.env.DATABASE_URL,
         ssl: {
-            rejectUnauthorized: false
+            rejectUnauthorized: true
         }
     });
 }
